c88aad71851a75e2a6cde3d0666f60a7c8ef9c08
Amirtha24052002 <amy24052002@gmail.com>
2022-09-11 11:19:04 +0530

    My second Commit Message after modification

--- Add.java
+++ Add.java
@@ -4,5 +4,7 @@ class Add{
 		int a=20;
 		int b=20;
 		System.out.println("Addition of Two numbers :"+(a+b));
+		System.out.prinltn("Welcome to Cloud Computing Lab");
+		System.out.println("My first Changes to be Commited");
 	}
 }
